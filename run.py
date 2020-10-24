ac379a6b1ff2ecdb79023c1d87545eff7e3a1c50
Kokoserver <owonikokoolaluwa@gmail.com>
2020-10-24 17:10:26 +0100

    ashsd

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("lionheart:app", debug=True, reload=True, workers=)
+    uvicorn.run("lionheart:app", debug=True, reload=True, workers=4)
