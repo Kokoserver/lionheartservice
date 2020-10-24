43ae43258f5f93d3d39020c7e88cecb1f533413c
Kokoserver <owonikokoolaluwa@gmail.com>
2020-10-24 17:07:35 +0100

    pushing  it

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("lionheart:app", debug=True, reload=True, workers=3)
+    uvicorn.run("lionheart:app", debug=True, reload=True, workers=)
