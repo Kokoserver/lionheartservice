e557cfe741cca6d7fd7cc85657dfe443cd83340f
Kokoserver <owonikokoolaluwa@gmail.com>
2020-11-12 17:41:30 +0100

    mail

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ async def register(request:Request):
     new_user = User(firstname, lastname, email, category, phone, address)
     new_user.save()
     username = f"{firstname} {lastname}"
-    task = BackgroundTask(sendmail, UserEmail=email, username=username, category="register", conact_message=None )
-    return RedirectResponse(url="/homeresponse/23478638726", status_code=301, background=task)
+    sendmail(UserEmail=email, username=username, category="register", conact_message=None )
+    return RedirectResponse(url="/homeresponse/23478638726", status_code=301)
 
 async def adminTemplate(request:Request):
     return template("pages/admin.html", {"request":request})
@@ -120,8 +120,8 @@ async def contact(request:Request):
     newContact = new_contact.save()
     contact = f"name:{name}\n\n email:{email}\n\nphone:{phone}\n\n:message:{message}"
     subject = "New contact message"
-    task = BackgroundTask(sendmail, UserEmail=email, username=name, category="contact",conact_message=contact )
+    sendmail(UserEmail=email, username=name, category="contact",conact_message=contact )
     return template("pages/index.html", { "request":request, "status":"success", 
-    "message":f"Thanks for contacting us {name}"}, background=task)
+    "message":f"Thanks for contacting us {name}"})
     
 
